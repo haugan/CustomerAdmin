5b820c636ce66443305b5d995b3c76a11fc9af93
Marius Riis Haugan <hauganmarius@outlook.com>
2018-04-24 16:28:20 +0200

    Removed and sorted usings

--- CustomerAdmin/Startup.cs
+++ CustomerAdmin/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using CustomerAdmin.Data.Models.DbContexts;
+﻿using CustomerAdmin.Data.Models.DbContexts;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
